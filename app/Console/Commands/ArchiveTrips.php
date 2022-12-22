28666c266ca587f0ada827a4365d23e8d27d293d
Zahid Ali <za_keerio@yahoo.com>
2022-12-23 01:13:42 +0500

    added schedular/cronjob for updating the Trips to Archieved for the last dates

---
+++ app/Console/Commands/ArchiveTrips.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Log;
+
+
+class ArchiveTrips extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:archivetrips';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // return 0;
+        DB::table("trips")->where('departuredate', '<' , date('Y-m-d'))->update(['archived' => 'Y']);
+
+        // Log::info("Worked here");
+
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,9 +4,17 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        Commands\ArchiveTrips::class
+    ];
+
+
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +23,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('update:archivetrips')->daily();
+
     }
 
     /**
