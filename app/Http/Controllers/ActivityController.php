029e566bdb9aea6f497e849cd22e07ab1c6868a3
Zahid Ali <za_keerio@yahoo.com>
2023-01-18 22:52:57 +0500

    updated my-activities not archived to crewmember

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -878,7 +878,7 @@ class ActivityController extends Controller
             ->join('tripcrews', 'tripnumber', '=', 'trips.id')
             ->select('tripcrews.*', 'trips.*')
             ->where('crewcode', '=', Session::get('initials'))
-            // ->where('trips.archived', '!=', 'Y')
+            ->where('trips.archived', '!=', 'Y')
 
             ->where('tripcrews.confirmed', '=', 'Y')
             // ->where('tripcrews.isskipper','!=','Y')
